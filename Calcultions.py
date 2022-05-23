6cde8d00f11f26a8fcfd89cac0f2b21f9f6191fd
Dennis Mulder <dnns.mldr@gmail.com>
2022-05-23 22:14:04 +0200

    Initial commit

---
+++ Calcultions.py
@@ -0,0 +1,37 @@
+def ma(input_list, length):
+    """
+    Moving average
+    :param input_list: is the list of closing prices
+    :param length: is the backtrack-length.
+    :return: the average of the closing prices is the backtrack-length
+    """
+    if length == 24:
+        short_list = input_list
+    else:
+        short_list = input_list[23 - length:-1:]
+    length_list = len(short_list)
+    summery = float(0.0)
+    for j in short_list:
+        summery += float(j)
+    avg = summery / length_list
+    return avg
+
+
+def sma_trade_logic_hourly_oneday(bar_list):
+    """
+    Calculation
+    :param bar_list: is binance trading set data
+    :return: list of hourly of past 24h closing prices
+    """
+    closing_list = []
+    for i in bar_list:
+        closing_list.append(i[4])
+    return closing_list
+
+
+def log(log_list):
+    final_string = ",".join(log_list)
+    with open('log.csv', 'a') as f:
+        f.write('\n')
+        f.write(final_string)
+    return final_string
---
+++ main.py
@@ -0,0 +1,60 @@
+import Constants
+from Calcultions import sma_trade_logic_hourly_oneday, ma, log
+from binance import Client
+import binance.enums
+from datetime import datetime
+
+
+# open connection with api
+client = Client(Constants.api_key, Constants.api_secret)
+
+# LUNA - BUSD closing last past 24h hourly
+bars = client.get_historical_klines('LUNABUSD', Client.KLINE_INTERVAL_1HOUR, "1 day ago UTC")
+closing_list = sma_trade_logic_hourly_oneday(bars)
+
+# BUSD free balance
+balance_BUSD_dict = client.get_asset_balance(asset='BUSD')
+balance_BUSD = float(balance_BUSD_dict['free'])
+
+# LUNA
+balance_LUNA_dict = client.get_asset_balance(asset='LUNA')
+balance_LUNA = float(balance_LUNA_dict['free'])
+prices = client.get_all_tickers()
+
+LUNA_price = 0.0
+for h in prices:
+    if h['symbol'] == "LUNABUSD":
+        LUNA_price = float(h['price'])
+buy_or_sell_amount = int((balance_BUSD - (0.02 * balance_BUSD)) / LUNA_price)
+
+# Determine the MA's
+ma_6 = round(ma(closing_list, 6), 8)
+ma_18 = round(ma(closing_list, 18), 8)
+
+# Buy or Sell? that's the question
+log_list = []
+if (ma_6 >= ma_18) & (balance_LUNA == 0):
+    # Buy order
+    buy_order = client.order_market_buy(
+        symbol='LUNABUSD',
+        quantity=buy_or_sell_amount)
+    log_list.append('Buy LUNA')
+elif (ma_6 > ma_18) & (balance_LUNA != 0):
+    # sell order
+    sell_order = client.order_market_sell(
+        symbol='LUNABUSD',
+        quantity=balance_LUNA)
+    log_list.append('Sell LUNA')
+else:
+    log_list.append('Do Nothing')
+
+
+# register al off the action
+log_list.append(str(buy_or_sell_amount))
+log_list.append(str(LUNA_price))
+log_list.append(str(ma_6))
+log_list.append(str(ma_18))
+log_list.append(str(balance_LUNA))
+log_list.append(str(balance_BUSD))
+log_list.append(str(datetime.now()))
+log(log_list)
